a0034a0efd986e4aeb52279b6e7fd456dcc8b796
Gibagood <63713936+Gibagood@users.noreply.github.com>
2020-06-25 17:30:00 +0300

    переделал фильтр сортировки, начал слайдер

--- js/slider.js
+++ js/slider.js
@@ -8,11 +8,29 @@ function nextSlide(){
     currentSlide = (currentSlide+1)%sliderList.length;
     sliderList[currentSlide].className = "slider-current";
 } */
+var sliders = document.querySelectorAll(".slider-list");
 var slider = document.querySelector(".slider");
+var toggleList = document.querySelectorAll(".toggle-list");
 var toggleButton = document.querySelector(".toggle-button");
+var siteWrapperOne = document.querySelector(".site-wrapper-1");
+var siteWrapperTwo = document.querySelector(".site-wrapper-2");
+var siteWrapperThree = document.querySelector(".site-wrapper-3");
 
-
-toggleButton.onclick = function () {
-    slider.classList.toggle("slider-current");
+toggleButton.addEventListener("click", function() {
+    for (i=0; i<=toggleList.length; i++){
+        if(toggleList[i].classList.contains("current")){
+            toggleList.classList.remove("current");
+            toggleButton.classList.add("current");
+        }
+    /* if(toggleList.contains("current")){
+        console.log(".toggleList");
+        toggleList.classList.remove("current");
+        toggleButton.classList.add("current");
+    };
+ */
+/* 
+    slider.classList.add("slider-current");
     toggleButton.classList.toggle("current");
-};
+ */
+  }  
+});
