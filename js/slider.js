517f8e58b8b55903e4f68bec3b71877e4c12a473
Gibagood <63713936+Gibagood@users.noreply.github.com>
2020-06-28 13:27:24 +0300

    Доделал слайдер

--- js/slider.js
+++ js/slider.js
@@ -1,47 +1,38 @@
-/* var sliderList = document.querySelectorAll(".slider-list");
-var currentSlide = 0;
-var slide-interval = setInterval(nextSlide,2000);
-
-
-function nextSlide(){
-    sliderList[currentSlide].className = "slider-item";
-    currentSlide = (currentSlide+1)%sliderList.length;
-    sliderList[currentSlide].className = "slider-current";
-} */
-var sliders = document.querySelectorAll(".slider-list");
-var slider = document.querySelector(".slider");
 var toggleButtons = document.querySelectorAll(".toggle-button");
-/* var toggleButton = document.querySelector(".toggle-button"); */
+var siteWrapper = document.querySelector(".site-wrapper");
 var siteWrapperOne = document.querySelector(".site-wrapper-1");
 var siteWrapperTwo = document.querySelector(".site-wrapper-2");
 var siteWrapperThree = document.querySelector(".site-wrapper-3");
+var toggleButtonOne = document.querySelector(".toggle-button-1");
+var toggleButtonTwo = document.querySelector(".toggle-button-2");
+var toggleButtonThree = document.querySelector(".toggle-button-3");
+var sliderItemOne = document.querySelector(".slider-item-1");
+var sliderItemTwo = document.querySelector(".slider-item-2");
+var sliderItemThree = document.querySelector(".slider-item-3");
 
 
     
 for (let toggleButton of toggleButtons){
     toggleButton.onclick = function () {
-        console.log(toggleButtons);
-        let i = 0;
-        while (i<=toggleButtons.length) {
-            toggleButton.classList.remove("current");
-            console.log(toggleButtons[i]);
-            i++;
-        };
+        document.querySelector(".current").classList.remove("current");
         toggleButton.classList.add("current");
-        
-        
-    } 
-    /* for (toggleButton of toggleButtons){
-        toggleButton.classList.add("current");
-    } */
-    /* if(toggleList.contains("current")){
-        console.log(".toggleList");
-        toggleList.classList.remove("current");
-        toggleButton.classList.add("current");
-    };
- */
-/* 
-    slider.classList.add("slider-current");
-    toggleButton.classList.toggle("current");
- */
+        if(toggleButtonOne.classList.contains("current")){
+            siteWrapper.classList.remove("site-wrapper-2" , "site-wrapper-3");
+            siteWrapper.classList.add("site-wrapper-1");
+            document.querySelector(".slider-current").classList.remove("slider-current");
+            sliderItemOne.classList.add("slider-current");
+        }
+        else  if(toggleButtonTwo.classList.contains("current")){
+            siteWrapper.classList.remove("site-wrapper-1" , "site-wrapper-3");
+            siteWrapper.classList.add("site-wrapper-2");
+            document.querySelector(".slider-current").classList.remove("slider-current");
+            sliderItemTwo.classList.add("slider-current");
+        }
+        else  if(toggleButtonThree.classList.contains("current")){
+            siteWrapper.classList.remove("site-wrapper-1" , "site-wrapper-2");
+            siteWrapper.classList.add("site-wrapper-3");
+            document.querySelector(".slider-current").classList.remove("slider-current");
+            sliderItemThree.classList.add("slider-current");
+        }
+    }
 }
